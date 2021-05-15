1e223b067e4e0fdde0b31d478c78ec73d5d2882f
Mateus Peixoto <mateuspeixoto.contato@gmail.com>
2021-05-15 15:36:34 -0300

    consumo api chuck

--- src/views/pages/home/Home.js
+++ src/views/pages/home/Home.js
@@ -1,17 +1,44 @@
+import { api } from '../../../service/api';
+
+const getJokesRandom = async () => {
+  const request = await api.get('random');
+  return request.data;
+};
+
+const getJokesCategory = async () => {
+  const request = await api.get('categories');
+  return request.data;
+};
+
 let Home = {
   is_private: false,
 
   render: async () => {
-      let view = `
+    const jokes = await getJokesRandom();
+    const categories = await getJokesCategory();
+
+    let view = `
           <div>
-            <h1>Home</div>
+            <h1>Categorias:</h1>
+            <ul class="menu-list">
+              ${categories
+                .map(
+                  (category, index) =>
+                    `<li class="menu-item" key=${index}>${category}</li>`
+                )
+                .join('')}
+            </ul>
+            <br>
+            <img src=${jokes.icon_url}>
+            <h2>Fatos de Chuck Norris</h2>
+            <p>— "${jokes.value}"</p>
           </div>
       `;
 
-      return view
+    return view;
   },
 
-  after_render: async () => {}
-}
+  after_render: async () => {},
+};
 
 export default Home;
